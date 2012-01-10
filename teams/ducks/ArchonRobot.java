58b2ab8678ff9b3cb11355a43637cc9aaf81983d
Cory Li <51144+Cixelyn@users.noreply.github.com>
2012-01-10 13:32:33 -0500

    committing base robot class

---
+++ teams/ducks/ArchonRobot.java
@@ -0,0 +1,16 @@
+package ducks;
+
+import battlecode.common.RobotController;
+
+public class ArchonRobot extends BaseRobot{
+
+	public ArchonRobot(RobotController myRC) {
+		super(myRC);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
