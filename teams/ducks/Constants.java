cc0fc4aa32d687649e00e6a886e445c7f1d3255b
Justin Venezuela <jven@mit.edu>
2012-01-11 00:36:37 -0500

    Moved constants from BaseRobot to Constants

--- teams/ducks/Constants.java
+++ teams/ducks/Constants.java
@@ -15,6 +15,16 @@ public final class Constants {
 		Direction.NORTH_WEST,
 	};
 	
-	
+	// Archons must have this amount of flux in order to distribute.
+	public static final double MIN_ARCHON_FLUX = 0.2;
+	// Archons try to maintain this amount of flux on other units.
+	public static final double MIN_UNIT_FLUX = 20;
+	// Units below this amount of flux will not execute their run method.
+	public static final double POWER_DOWN_FLUX = 1;
+	// The frequency with which to broadcast.
+	public static final int BROADCAST_FREQUENCY = 5;
+	// The number of damaged units that a scout must have in range in order
+	// to regen.
+	public static final int MIN_DAMAGED_UNITS_TO_REGEN = 1;
 	
 }
