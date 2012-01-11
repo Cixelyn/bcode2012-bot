c97f87f7e68a4302c900733a4275e4f2d94578c6
DarkArchon <3052946+DarkArchonMind@users.noreply.github.com>
2012-01-10 23:57:21 -0500

    util/constants

---
+++ teams/ducks/Constants.java
@@ -0,0 +1,20 @@
+package ducks;
+
+import battlecode.common.Direction;
+
+public final class Constants {
+	
+	public static final Direction[] directions = new Direction[] {
+		Direction.NORTH,
+		Direction.NORTH_EAST,
+		Direction.EAST,
+		Direction.SOUTH_EAST,
+		Direction.SOUTH,
+		Direction.SOUTH_WEST,
+		Direction.WEST,
+		Direction.NORTH_WEST,
+	};
+	
+	
+	
+}
