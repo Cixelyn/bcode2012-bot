e2eb5ee3246004e5b7425db8370e9a970d4c0dda
DarkArchon <3052946+DarkArchonMind@users.noreply.github.com>
2012-01-12 18:55:46 -0500

    merging optimized sensing and bug nav into navigator

--- teams/ducks/BlindBug.java
+++ teams/ducks/BlindBug.java
@@ -24,7 +24,399 @@ public class BlindBug extends Navigation {
 		super(myBR);
 	}
 	
+	/**
+	 * Bug nav, only using terrain, does not take into account obstacles like other robots
+	 */
 	public Direction navigateTo(MapLocation target) throws GameActionException
+	{
+		if (rc.isMovementActive()) {
+			return Direction.NONE;
+		}
+		
+		if (!target.equals(bugTarget))
+		{
+			bugTarget = target;
+			bugStart = null;
+			bugGing = false;
+			bugRoundStart = 0;
+			bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+			bugDist = 9999;
+			bugFlipped = false;
+		}
+		
+		if (br.currLoc.equals(target)) return Direction.NONE;
+		
+		Direction toTarget = br.currLoc.directionTo(target);
+		
+		if (br.currLoc.isAdjacentTo(target))
+		{
+			return toTarget;
+		}
+		
+//		boolean[] moveable = br.dc.getMovableDirections();
+		
+
+		rc.setIndicatorString(1, "start:"+bugStart+" end:"+bugTarget+" cw:"+bugCW+" cur:"+br.currLoc+" obs:"+bugObs+" bugging:"+bugGing);
+		rc.setIndicatorString(2, "rstart:"+bugRoundStart+" stopthreshold:"+bugStopThreshold);
+		
+		int x=0;
+		boolean[] moveableland = br.dc.getMovableLand();
+		while (true)
+		{
+			if (x++>3)
+			{
+				x++;
+				return br.currDir.opposite();
+			}
+			if (bugGing)
+			{
+				if (!bugFlipped)
+				{
+					boolean stopbugging = false;
+					if (br.currRound-bugRoundStart > bugStopThreshold)
+					{
+						stopbugging = true;
+						bugCW = !bugCW;
+						bugRoundStart = br.currRound;
+						bugStopThreshold = bugStopThreshold*2;
+						continue;
+					} else if (bugTarget.x == bugStart.x)
+					{
+						if (br.currLoc.x == bugStart.x)
+						{
+							if (br.currLoc.y>bugStart.y)
+							{
+								if (br.currLoc.y<=bugTarget.y)
+								{
+									stopbugging = true;
+								}
+							} else if (br.currLoc.y<bugStart.y)
+							{
+								if (br.currLoc.y>=bugTarget.y)
+								{
+									stopbugging = true;
+								}
+							}
+						}
+					} else if (bugTarget.y == bugStart.y)
+					{
+						if (br.currLoc.y == bugStart.y)
+						{
+							if (br.currLoc.x>bugStart.x)
+							{
+								if (br.currLoc.x<=bugTarget.x)
+								{
+									stopbugging = true;
+								}
+							} else if (br.currLoc.x<bugStart.x)
+							{
+								if (br.currLoc.x>=bugTarget.x)
+								{
+									stopbugging = true;
+								}
+							}
+						}
+					} else
+					{
+						if (bugSlope>0)
+						{
+							if (bugSlope>1)
+							{
+								if (bugTarget.y>bugStart.y)
+								{
+									if (br.currLoc.y>bugStart.y)
+									{
+										double approxX = (br.currLoc.y-bugStart.y)/bugSlope+bugStart.x;
+										int a = (int)approxX;
+										if (br.currLoc.x == a || br.currLoc.x == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								} else
+								{
+									if (br.currLoc.y<bugStart.y)
+									{
+										double approxX = (br.currLoc.y-bugStart.y)/bugSlope+bugStart.x;
+										int a = (int)approxX;
+										if (br.currLoc.x == a || br.currLoc.x == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								}
+							} else //bugSlope<1
+							{
+								if (bugTarget.x>bugStart.x)
+								{
+									if (br.currLoc.x>bugStart.x)
+									{
+										double approxY = bugSlope*(br.currLoc.x-bugStart.x)+bugStart.y;
+										int a = (int)approxY;
+										if (br.currLoc.y == a || br.currLoc.y == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								} else
+								{
+									if (br.currLoc.x<bugStart.x)
+									{
+										double approxY = bugSlope*(br.currLoc.x-bugStart.x)+bugStart.y;
+										int a = (int)approxY;
+										if (br.currLoc.y == a || br.currLoc.y == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								}
+							}
+						} else // bugSlope<0
+						{
+							if (bugSlope>-1)
+							{
+								if (bugTarget.x>bugStart.x)
+								{
+									if (br.currLoc.x>bugStart.x)
+									{
+										double approxY = bugSlope*(br.currLoc.x-bugStart.x)+bugStart.y;
+										int a = (int)approxY;
+										if (br.currLoc.y == a || br.currLoc.y == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								} else
+								{
+									if (br.currLoc.x<bugStart.x)
+									{
+										double approxY = bugSlope*(br.currLoc.x-bugStart.x)+bugStart.y;
+										int a = (int)approxY;
+										if (br.currLoc.y == a || br.currLoc.y == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								}
+							} else //bugSlope<-1
+							{
+								if (bugTarget.y>bugStart.y)
+								{
+									if (br.currLoc.y>bugStart.y)
+									{
+										double approxX = (br.currLoc.y-bugStart.y)/bugSlope+bugStart.x;
+										int a = (int)approxX;
+										if (br.currLoc.x == a || br.currLoc.x == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								} else
+								{
+									if (br.currLoc.y<bugStart.y)
+									{
+										double approxX = (br.currLoc.y-bugStart.y)/bugSlope+bugStart.x;
+										int a = (int)approxX;
+										if (br.currLoc.x == a || br.currLoc.x == a+1)
+										{
+											stopbugging = true;
+										} 
+									}
+								}
+							}
+						}
+					}
+					if (!stopbugging )
+					{
+						if (moveableland[toTarget.ordinal()])
+						{
+							int dist = br.currLoc.distanceSquaredTo(bugTarget);
+							if (dist<bugDist)
+							{
+								bugDist = dist;
+								stopbugging = true;
+							}
+						} else if (rc.senseTerrainTile(br.currLoc.add(bugObs))==TerrainTile.OFF_MAP)
+						{
+							bugCW = !bugCW;
+							bugRoundStart = br.currRound;
+							bugStopThreshold = 99999;
+							bugFlipped = true;
+							continue;
+						}
+						
+					}
+					if (stopbugging || moveableland[bugObs.ordinal()])
+					{
+						bugGing = false;
+						bugCW = !bugCW;
+						bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+						bugFlipped = true;
+						continue;
+					}
+				}
+				
+//				if (	moveableland[0] || moveableland[1] || moveableland[2] || moveableland[3] || 
+//						moveableland[4] || moveableland[5] || moveableland[6] || moveableland[7])
+				if (true)
+				{
+					if (bugCW)
+					{
+//						int dir = toTarget.ordinal();
+						int odir = bugObs.ordinal();
+						int dir = odir;
+						while (!moveableland[dir]) dir = (dir+7)%8;
+						if (br.currDir.equals(Constants.directions[dir]))
+						{
+//							rc.moveForward();
+							bugObs = br.currLoc.add(br.currDir).directionTo(br.currLoc.add(br.currDir.rotateRight()));
+							return Constants.directions[dir];
+						} else
+						{
+//							rc.setDirection(Constants.directions[dir]);
+							return Constants.directions[dir];
+						}
+					} else
+					{
+//						int dir = toTarget.ordinal();
+						int odir = bugObs.ordinal();
+						int dir = odir;
+						while (!moveableland[dir]) dir = (dir+1)%8;
+						if (br.currDir.equals(Constants.directions[dir]))
+						{
+//							rc.moveForward();
+							bugObs = br.currLoc.add(br.currDir).directionTo(br.currLoc.add(br.currDir.rotateLeft()));
+							return Constants.directions[dir];
+						} else
+						{
+//							rc.setDirection(Constants.directions[dir]);
+							return Constants.directions[dir];
+						}
+					}
+				} else return Direction.NONE;
+			} else //if (!bugGing)
+			{
+				if (br.currDir == toTarget)
+				{
+					if (moveableland[toTarget.ordinal()])
+					{
+//						rc.moveForward();
+						return toTarget;
+					} else if (moveableland[toTarget.rotateLeft().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateLeft());
+						return toTarget.rotateLeft();
+					} else if (moveableland[toTarget.rotateRight().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateRight());
+						return toTarget.rotateRight();
+					} else
+					{
+						bugGing = true;
+						bugObs = toTarget;
+						bugStart = br.currLoc;
+						bugRoundStart = br.currRound;
+						
+						bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+						bugDist = br.currLoc.distanceSquaredTo(bugTarget);
+						bugFlipped = false;
+						
+						if (bugTarget.x!=bugStart.x)
+							bugSlope = (bugTarget.y-bugStart.y)/(bugTarget.x-bugStart.x);
+					}
+				} else if (br.currDir == toTarget.rotateLeft())
+				{
+					if (moveableland[br.currDir.ordinal()])
+					{
+//						rc.moveForward();
+						return br.currDir;
+					} else if (moveableland[toTarget.ordinal()])
+					{
+//						rc.setDirection(toTarget);
+						return toTarget;
+					} else if (moveableland[toTarget.rotateRight().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateRight());
+						return toTarget.rotateRight();
+					} else
+					{
+						bugGing = true;
+						bugObs = toTarget;
+						bugStart = br.currLoc;
+						bugRoundStart = br.currRound;
+						
+						bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+						bugDist = br.currLoc.distanceSquaredTo(bugTarget);
+						bugFlipped = false;
+						
+						if (bugTarget.x!=bugStart.x)
+							bugSlope = (bugTarget.y-bugStart.y)/(bugTarget.x-bugStart.x);
+					}
+				} else if (br.currDir == toTarget.rotateRight())
+				{
+					if (moveableland[br.currDir.ordinal()])
+					{
+//						rc.moveForward();
+						return br.currDir;
+					} else if (moveableland[toTarget.ordinal()])
+					{
+//						rc.setDirection(toTarget);
+						return toTarget;
+					} else if (moveableland[toTarget.rotateLeft().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateLeft());
+						return toTarget.rotateLeft();
+					} else
+					{
+						bugGing = true;
+						bugObs = toTarget;
+						bugStart = br.currLoc;
+						bugRoundStart = br.currRound;
+						
+						bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+						bugDist = br.currLoc.distanceSquaredTo(bugTarget);
+						bugFlipped = false;
+						
+						if (bugTarget.x!=bugStart.x)
+							bugSlope = (bugTarget.y-bugStart.y)/(bugTarget.x-bugStart.x);
+					}
+				} else
+				{
+					if (moveableland[toTarget.ordinal()])
+					{
+//						rc.setDirection(toTarget);
+						return toTarget;
+					} else if (moveableland[toTarget.rotateLeft().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateLeft());
+						return toTarget.rotateLeft();
+					} else if (moveableland[toTarget.rotateRight().ordinal()])
+					{
+//						rc.setDirection(toTarget.rotateRight());
+						return toTarget.rotateRight();
+					} else
+					{
+						bugGing = true;
+						bugObs = toTarget;
+						bugStart = br.currLoc;
+						bugRoundStart = br.currRound;
+						
+						bugStopThreshold = BUG_ROUNDS_BEFORE_STOPPING_INITIAL;
+						bugDist = br.currLoc.distanceSquaredTo(bugTarget);
+						bugFlipped = false;
+						
+						if (bugTarget.x!=bugStart.x)
+							bugSlope = (bugTarget.y-bugStart.y)/(bugTarget.x-bugStart.x);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * bug nav that takes into account other robots
+	 */
+	public Direction navigateToFull(MapLocation target) throws GameActionException
 	{
 		if (rc.isMovementActive()) {
 			return Direction.NONE;
--- teams/ducks/MapCache.java
+++ teams/ducks/MapCache.java
@@ -26,6 +26,8 @@ public class MapCache {
 	/** The boundaries of tiles we have confirmed are ground tiles. */
 	int minXGround, maxXGround, minYGround, maxYGround;
 	int senseDist;
+	/** optimized sensing list for each unit */
+	private final int[][][] optimizedSensingList;
 	public MapCache(BaseRobot baseRobot) {
 		isWall = new boolean[MAP_SIZE][MAP_SIZE];
 		sensed = new boolean[MAP_SIZE][MAP_SIZE];
@@ -39,6 +41,16 @@ public class MapCache {
 		maxYGround = powerCoreWorldY + 1;
 		senseDist = baseRobot.myType==RobotType.ARCHON ? 5 :
 			baseRobot.myType==RobotType.SCOUT ? 4 : 3;
+		switch(baseRobot.myType)
+		{
+		case ARCHON: optimizedSensingList = sensorRangeARCHON; break;
+		case SCOUT: optimizedSensingList = sensorRangeSCOUT; break;
+		case DISRUPTER: optimizedSensingList = sensorRangeDISRUPTER; break;
+		case SCORCHER: optimizedSensingList = sensorRangeSCORCHER; break;
+		case SOLDIER: optimizedSensingList = sensorRangeSOLDIER; break;
+		default:
+			optimizedSensingList = new int[0][0][0];
+		}
 	}
 	
 	/** Senses most tiles around current location in range. 
@@ -66,7 +78,24 @@ public class MapCache {
 	 * @param lastMoved the direction we just moved in
 	 */
 	public void senseTilesOptimized(Direction lastMoved) {
-		//TODO implement
+		if (lastMoved.ordinal()>7) return;
+		final int[][] list = optimizedSensingList[lastMoved.ordinal()];
+		MapLocation myLoc = baseRobot.currLoc;
+		int x = worldToCacheX(myLoc.x);
+		int y = worldToCacheY(myLoc.y);
+		for (int i=0; i<list.length; i++)
+		{
+			int dx = list[i][0];
+			int dy = list[i][1];
+			if(sensed[x+dx][y+dy]) continue;
+			MapLocation loc = myLoc.add(dx, dy);
+			TerrainTile tt = baseRobot.rc.senseTerrainTile(loc);
+			if(tt!=null) {
+				isWall[x+dx][y+dy] = tt!=TerrainTile.LAND;
+				sensed[x+dx][y+dy] = true;
+			}
+			
+		}
 	}
 	
 	public int worldToCacheX(int worldX) {
@@ -82,4 +111,136 @@ public class MapCache {
 		return cacheY+powerCoreWorldY-POWER_CORE_POSITION;
 	}
 	
+	
+//	Magic arrays
+	private static final int[][][] sensorRangeARCHON = new int[][][] { //ARCHON
+		{ //NORTH
+			{-6,0},{-5,-3},{-4,-4},{-3,-5},{-2,-5},{-1,-5},{0,-6},{1,-5},{2,-5},{3,-5},{4,-4},{5,-3},{6,0},
+		},
+		{ //NORTH_EAST
+			{-3,-5},{-2,-5},{0,-6},{0,-5},{1,-5},{2,-5},{3,-5},{3,-4},{4,-4},{4,-3},{5,-3},{5,-2},{5,-1},{5,0},{5,2},{5,3},{6,0},
+		},
+		{ //EAST
+			{0,-6},{0,6},{3,-5},{3,5},{4,-4},{4,4},{5,-3},{5,-2},{5,-1},{5,1},{5,2},{5,3},{6,0},
+		},
+		{ //SOUTH_EAST
+			{-3,5},{-2,5},{0,5},{0,6},{1,5},{2,5},{3,4},{3,5},{4,3},{4,4},{5,-3},{5,-2},{5,0},{5,1},{5,2},{5,3},{6,0},
+		},
+		{ //SOUTH
+			{-6,0},{-5,3},{-4,4},{-3,5},{-2,5},{-1,5},{0,6},{1,5},{2,5},{3,5},{4,4},{5,3},{6,0},
+		},
+		{ //SOUTH_WEST
+			{-6,0},{-5,-3},{-5,-2},{-5,0},{-5,1},{-5,2},{-5,3},{-4,3},{-4,4},{-3,4},{-3,5},{-2,5},{-1,5},{0,5},{0,6},{2,5},{3,5},
+		},
+		{ //WEST
+			{-6,0},{-5,-3},{-5,-2},{-5,-1},{-5,1},{-5,2},{-5,3},{-4,-4},{-4,4},{-3,-5},{-3,5},{0,-6},{0,6},
+		},
+		{ //NORTH_WEST
+			{-6,0},{-5,-3},{-5,-2},{-5,-1},{-5,0},{-5,2},{-5,3},{-4,-4},{-4,-3},{-3,-5},{-3,-4},{-2,-5},{-1,-5},{0,-6},{0,-5},{2,-5},{3,-5},
+		},
+	};
+	private static final int[][][] sensorRangeSOLDIER = new int[][][] { //SOLDIER
+		{ //NORTH
+			{-3,-1},{-2,-2},{-1,-3},{0,-3},{1,-3},{2,-2},{3,-1},
+		},
+		{ //NORTH_EAST
+			{-1,-3},{0,-3},{1,-3},{1,-2},{2,-2},{2,-1},{3,-1},{3,0},{3,1},
+		},
+		{ //EAST
+			{1,-3},{1,3},{2,-2},{2,2},{3,-1},{3,0},{3,1},
+		},
+		{ //SOUTH_EAST
+			{-1,3},{0,3},{1,2},{1,3},{2,1},{2,2},{3,-1},{3,0},{3,1},
+		},
+		{ //SOUTH
+			{-3,1},{-2,2},{-1,3},{0,3},{1,3},{2,2},{3,1},
+		},
+		{ //SOUTH_WEST
+			{-3,-1},{-3,0},{-3,1},{-2,1},{-2,2},{-1,2},{-1,3},{0,3},{1,3},
+		},
+		{ //WEST
+			{-3,-1},{-3,0},{-3,1},{-2,-2},{-2,2},{-1,-3},{-1,3},
+		},
+		{ //NORTH_WEST
+			{-3,-1},{-3,0},{-3,1},{-2,-2},{-2,-1},{-1,-3},{-1,-2},{0,-3},{1,-3},
+		},
+	};
+	private static final int[][][] sensorRangeSCOUT = new int[][][] { //SCOUT
+		{ //NORTH
+			{-5,0},{-4,-3},{-3,-4},{-2,-4},{-1,-4},{0,-5},{1,-4},{2,-4},{3,-4},{4,-3},{5,0},
+		},
+		{ //NORTH_EAST
+			{-3,-4},{-2,-4},{0,-5},{0,-4},{1,-4},{2,-4},{3,-4},{3,-3},{4,-3},{4,-2},{4,-1},{4,0},{4,2},{4,3},{5,0},
+		},
+		{ //EAST
+			{0,-5},{0,5},{3,-4},{3,4},{4,-3},{4,-2},{4,-1},{4,1},{4,2},{4,3},{5,0},
+		},
+		{ //SOUTH_EAST
+			{-3,4},{-2,4},{0,4},{0,5},{1,4},{2,4},{3,3},{3,4},{4,-3},{4,-2},{4,0},{4,1},{4,2},{4,3},{5,0},
+		},
+		{ //SOUTH
+			{-5,0},{-4,3},{-3,4},{-2,4},{-1,4},{0,5},{1,4},{2,4},{3,4},{4,3},{5,0},
+		},
+		{ //SOUTH_WEST
+			{-5,0},{-4,-3},{-4,-2},{-4,0},{-4,1},{-4,2},{-4,3},{-3,3},{-3,4},{-2,4},{-1,4},{0,4},{0,5},{2,4},{3,4},
+		},
+		{ //WEST
+			{-5,0},{-4,-3},{-4,-2},{-4,-1},{-4,1},{-4,2},{-4,3},{-3,-4},{-3,4},{0,-5},{0,5},
+		},
+		{ //NORTH_WEST
+			{-5,0},{-4,-3},{-4,-2},{-4,-1},{-4,0},{-4,2},{-4,3},{-3,-4},{-3,-3},{-2,-4},{-1,-4},{0,-5},{0,-4},{2,-4},{3,-4},
+		},
+	};
+	private static final int[][][] sensorRangeDISRUPTER = new int[][][] { //DISRUPTER
+		{ //NORTH
+			{-4,0},{-3,-2},{-2,-3},{-1,-3},{0,-4},{1,-3},{2,-3},{3,-2},{4,0},
+		},
+		{ //NORTH_EAST
+			{-2,-3},{0,-4},{0,-3},{1,-3},{2,-3},{2,-2},{3,-2},{3,-1},{3,0},{3,2},{4,0},
+		},
+		{ //EAST
+			{0,-4},{0,4},{2,-3},{2,3},{3,-2},{3,-1},{3,1},{3,2},{4,0},
+		},
+		{ //SOUTH_EAST
+			{-2,3},{0,3},{0,4},{1,3},{2,2},{2,3},{3,-2},{3,0},{3,1},{3,2},{4,0},
+		},
+		{ //SOUTH
+			{-4,0},{-3,2},{-2,3},{-1,3},{0,4},{1,3},{2,3},{3,2},{4,0},
+		},
+		{ //SOUTH_WEST
+			{-4,0},{-3,-2},{-3,0},{-3,1},{-3,2},{-2,2},{-2,3},{-1,3},{0,3},{0,4},{2,3},
+		},
+		{ //WEST
+			{-4,0},{-3,-2},{-3,-1},{-3,1},{-3,2},{-2,-3},{-2,3},{0,-4},{0,4},
+		},
+		{ //NORTH_WEST
+			{-4,0},{-3,-2},{-3,-1},{-3,0},{-3,2},{-2,-3},{-2,-2},{-1,-3},{0,-4},{0,-3},{2,-3},
+		},
+	};
+	private static final int[][][] sensorRangeSCORCHER = new int[][][] { //SCORCHER
+		{ //NORTH
+			{-2,-2},{-1,-3},{0,-3},{1,-3},{2,-2},
+		},
+		{ //NORTH_EAST
+			{-1,-3},{0,-3},{1,-3},{1,-2},{2,-2},{2,-1},{3,-1},{3,0},{3,1},
+		},
+		{ //EAST
+			{2,-2},{2,2},{3,-1},{3,0},{3,1},
+		},
+		{ //SOUTH_EAST
+			{-1,3},{0,3},{1,2},{1,3},{2,1},{2,2},{3,-1},{3,0},{3,1},
+		},
+		{ //SOUTH
+			{-2,2},{-1,3},{0,3},{1,3},{2,2},
+		},
+		{ //SOUTH_WEST
+			{-3,-1},{-3,0},{-3,1},{-2,1},{-2,2},{-1,2},{-1,3},{0,3},{1,3},
+		},
+		{ //WEST
+			{-3,-1},{-3,0},{-3,1},{-2,-2},{-2,2},
+		},
+		{ //NORTH_WEST
+			{-3,-1},{-3,0},{-3,1},{-2,-2},{-2,-1},{-1,-3},{-1,-2},{0,-3},{1,-3},
+		},
+	};
 }
