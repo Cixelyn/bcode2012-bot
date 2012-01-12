8753d13631a2c9d5b0aa8529041c3a996f8ecc24
hmao5 <hmao5@localhost>
2012-01-12 18:09:18 -0500

    wooz

--- teams/ducks/Navigator.java
+++ teams/ducks/Navigator.java
@@ -4,42 +4,53 @@ import battlecode.common.Direction;
 import battlecode.common.MapLocation;
 
 public class Navigator {
+	private final BaseRobot baseRobot;
 	private final MapCache mapCache; 
 	private final TangentBug tangentBug;
 	private final MapLocation zeroLoc;
-	private final Direction[] allDirections;
-	public Navigator(MapCache mapCache) {
-		this.mapCache = mapCache;
-		tangentBug = new TangentBug();
+	public Navigator(BaseRobot baseRobot) {
+		this.baseRobot = baseRobot;
+		mapCache = baseRobot.mc;
+		tangentBug = new TangentBug(this);
 		zeroLoc = new MapLocation(0,0);
-		allDirections = Direction.values();
 	}
 	
-	private Direction getRandomDirection() {
-		return allDirections[(int)(Math.random()*8)];
+	/** Resets the navigator, clearing it of any state. */
+	public void reset() {
+		tangentBug.reset();
 	}
+	
 	private Direction dxdyToDirection(int dx, int dy) {
 		return zeroLoc.directionTo(zeroLoc.add(dx, dy));
 	}
-	public Direction navigateTangentBug(MapLocation source, MapLocation destination) {
+	public Direction navigateTangentBug(MapLocation destination) {
 		int[] d = tangentBug.computeMove(mapCache.isWall, 
-				mapCache.worldToCacheX(source.x), mapCache.worldToCacheY(source.y), 
+				mapCache.worldToCacheX(baseRobot.currLoc.x), mapCache.worldToCacheY(baseRobot.currLoc.y), 
 				mapCache.worldToCacheX(destination.x), mapCache.worldToCacheY(destination.y));
 		return dxdyToDirection(d[0], d[1]);
 	}
-	public Direction navigateCompletelyRandomly(MapLocation source, MapLocation destination) {
-		return getRandomDirection();
+	public Direction navigateCompletelyRandomly() {
+		Direction dir;
+		int tries = 0;
+		do {
+			tries++;
+			if(tries>32) return Direction.NONE;
+			dir = NavigatorUtilities.getRandomDirection();
+		} while(baseRobot.rc.canMove(dir));
+		return dir;
 	}
-	public Direction navigateBug(MapLocation source, MapLocation destination) {
+	public Direction navigateBug(MapLocation destination) {
 		//TODO implement
-		return getRandomDirection();
+		return navigateCompletelyRandomly();
 	}
-	public Direction navigateDirectionalBug(MapLocation source, MapLocation destination) {
+	public Direction navigateDirectionalBug(Direction dir, boolean traceClockwise) {
 		//TODO implement
-		return getRandomDirection();
+		return dir;
 	}
-	public Direction navigateDStar(MapLocation source, MapLocation destination) {
+	public Direction navigateDStar(MapLocation destination) {
 		//TODO implement
-		return getRandomDirection();
+		return navigateCompletelyRandomly();
 	}
+	
+	
 }
