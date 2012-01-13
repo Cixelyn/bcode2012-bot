c80464f0c4a3d605d058d26900e19241de20c027
Justin Venezuela <jven@jvenezue.la>
2012-01-13 02:35:53 -0500

    Ported fibbyBot2 strat back into ducks with new nav code

---
+++ teams/ducks/SoldierRobotJV.java
@@ -0,0 +1,15 @@
+package ducks;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class SoldierRobotJV extends BaseRobot {
+
+	public SoldierRobotJV(RobotController myRC) {
+		super(myRC);
+	}
+
+	@Override
+	public void run() throws GameActionException {
+	}
+}
