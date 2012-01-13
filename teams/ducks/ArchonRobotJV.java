2efca5bcaf2bc2b2330fe8f247672831bec6d967
Justin Venezuela <jven@jvenezue.la>
2012-01-13 15:22:01 -0500

    Moved getClosestEnemy and isTowerTargetable code to DataCache... archonJV kites enemies

--- teams/ducks/ArchonRobotJV.java
+++ teams/ducks/ArchonRobotJV.java
@@ -2,7 +2,6 @@ package ducks;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
-import battlecode.common.GameConstants;
 import battlecode.common.GameObject;
 import battlecode.common.MapLocation;
 import battlecode.common.Robot;
@@ -23,7 +22,6 @@ public class ArchonRobotJV extends BaseRobot {
 	
 	public ArchonRobotJV(RobotController myRC) {
 		super(myRC);
-		nav.setNavigationMode(NavigationMode.TANGENT_BUG);
 		currState = RobotState.INITIALIZE;
 	}
 	
@@ -42,6 +40,9 @@ public class ArchonRobotJV extends BaseRobot {
 			case RUSH:
 				rush();
 				break;
+			case CHASE:
+				chase();
+				break;
 			case GOTO_POWER_CORE:
 				gotoPowerCore();
 				break;
@@ -52,7 +53,10 @@ public class ArchonRobotJV extends BaseRobot {
 	}
 	
 	public void initialize() throws GameActionException {
+		// set nav mode
+		nav.setNavigationMode(NavigationMode.TANGENT_BUG);
 		// determine leader
+		// TODO(jven): check for leader in other states in case leader dies
 		if (currLoc.equals(dc.getAlliedArchons()[0])) {
 			isLeader = true;
 		}
@@ -133,6 +137,9 @@ public class ArchonRobotJV extends BaseRobot {
 			}
 		}
 		if (soldiersSpawned >= Constants.SOLDIERS_PER_ARCHON) {
+			// wake up units
+			wakeUpSoldiers();
+			// change state
 			if (isLeader) {
 				currState = RobotState.GOTO_POWER_CORE;
 			} else {
@@ -158,6 +165,27 @@ public class ArchonRobotJV extends BaseRobot {
 			bearing = bearing.rotateLeft().rotateLeft();
 			explorationTarget = currLoc.add(bearing, 100);
 		}
+		// distribute flux
+		distributeFlux();
+		// chase enemy if in range
+		if (dc.getClosestEnemy() != null) {
+			currState = RobotState.CHASE;
+		}
+	}
+	
+	public void chase() throws GameActionException {
+		// go back to previous state if no enemy in range
+		RobotInfo closestEnemy = dc.getClosestEnemy();
+		if (closestEnemy == null) {
+			if (isLeader) {
+				currState = RobotState.GOTO_POWER_CORE;
+			} else {
+				currState = RobotState.RUSH;
+			}
+			return;
+		}
+		// kite enemy
+		kite(closestEnemy.location);
 		// distribute flux
 		distributeFlux();
 	}
@@ -186,6 +214,7 @@ public class ArchonRobotJV extends BaseRobot {
 		// move towards target
 		Direction dir = nav.navigateTo(target);
 		if (dir != Direction.OMNI && dir != Direction.NONE) {
+			/*
 			Direction[] wiggleDirs = new Direction[] {
 					dir,
 					dir.rotateLeft(),
@@ -205,47 +234,102 @@ public class ArchonRobotJV extends BaseRobot {
 					return;
 				}
 			}
+			*/
+			for (int tries = 0; tries < 256; tries++) {
+				if (!rc.canMove(dir)) {
+					if (Math.random() < 0.5) {
+						dir = dir.rotateLeft();
+					} else {
+						dir = dir.rotateRight();
+					}
+				} else {
+					break;
+				}
+			}
+			if (rc.canMove(dir)) {
+				if (currDir != dir.opposite()) {
+					rc.setDirection(dir.opposite());
+				} else {
+					rc.moveBackward();
+				}
+			}
+		}
+	}
+	
+	private void kite(MapLocation target) throws GameActionException {
+		// wait if movement is active
+		if (rc.isMovementActive()) {
+			return;
+		}
+		// turn in direction of target
+		Direction dir = currLoc.directionTo(target);
+		if (currDir != dir) {
+			rc.setDirection(dir);
+			return;
+		}
+		// stay at distance
+		int distance = currLoc.distanceSquaredTo(target);
+		if (distance < Constants.ARCHON_SAFETY_RANGE) {
+			if (rc.canMove(currDir.opposite())) {
+				rc.moveBackward();
+			}
+		} else {
+			if (rc.canMove(currDir)) {
+				rc.moveForward();
+			}
 		}
 	}
 	
 	private boolean spawnUnitInDir(
 			RobotType type, Direction dir) throws GameActionException {
-		if (rc.isMovementActive()) {
+		// wait if movement is active
+		if (rc.isMovementActive()) {	
 			return false;
-		} else if (currDir != dir) {
+		}
+		// turn in direction to spawn
+		if (currDir != dir) {
 			rc.setDirection(dir);
 			return false;
-		} else if (currFlux < type.spawnCost) {
-			return false;
-		} else if (dc.getAdjacentGameObject(dir, type.level) != null) {
-			return false;
-		} else {
-			rc.spawn(type);
-			currFlux -= type.spawnCost;
-			return true;
 		}
+		// wait if not enough flux
+		if (currFlux < type.spawnCost) {
+			return false;
+		}
+		// wait if unit is in the way
+		if (dc.getAdjacentGameObject(dir, type.level) != null) {
+			return false;
+		}
+		// spawn unit
+		rc.spawn(type);
+		currFlux -= type.spawnCost;
+		return true;
 	}
 	
 	private void distributeFlux() throws GameActionException {
 		// check all directions around you, ground and air
 		for (Direction d : Direction.values()) {
+			// ignore none direction
 			if (d == Direction.NONE) {
 				continue;
 			}
 			for (RobotLevel level : RobotLevel.values()) {
+				// if we don't have flux to give, abort
+				if (this.currFlux < Constants.MIN_ARCHON_FLUX) {
+					break;
+				}
+				// ignore power node level
 				if (level == RobotLevel.POWER_NODE) {
 					continue;
 				}
-				if (d == Direction.OMNI && level == RobotLevel.IN_AIR) {
+				// can't give flux to yourself, silly!
+				if (d == Direction.OMNI && level == RobotLevel.ON_GROUND) {
 					continue;
-				}
-				if (this.currFlux < Constants.MIN_ARCHON_FLUX) {
-					break;
 				}
 				GameObject obj = dc.getAdjacentGameObject(d, level);
 				if (obj instanceof Robot && obj.getTeam() == myTeam) {
 					// TODO(jven): data cache this?
 					RobotInfo rInfo = rc.senseRobotInfo((Robot)obj);
+					// don't give flux to towers
 					if (rInfo.type == RobotType.TOWER) {
 						continue;
 					}
@@ -261,10 +345,11 @@ public class ArchonRobotJV extends BaseRobot {
 						}
 					}
 				}
-				if (currFlux <= Constants.MIN_ARCHON_FLUX) {
-					return;
-				}
 			}
 		}
+	}
+	
+	private void wakeUpSoldiers() throws GameActionException {
+		io.sendInt("#sw", 0);
 	}
 }
