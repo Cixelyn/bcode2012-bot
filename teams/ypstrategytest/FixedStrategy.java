fb7edadd03884779b5b1aface6d7362d980235ae
DarkArchon <3052946+DarkArchonMind@users.noreply.github.com>
2012-01-14 23:57:10 -0500

    for cory to do his magical optimization skillz

--- teams/ypstrategytest/FixedStrategy.java
+++ teams/ypstrategytest/FixedStrategy.java
@@ -40,27 +40,30 @@ public abstract class FixedStrategy extends Strategy {
 	public void initForRound(int round) {
 //		newRoundIndex = 0;
 		newRoundSize = 0;
-		cleanUp(round);
+//		cleanUp(round);
+		cleanUp2(round);
 		StrategyElement temp;
 		
 		for (; baseIndex<baseStrategy.length; baseIndex++)
 		{
-			int bytecode = Clock.getBytecodeNum();
+//			int bytecode = Clock.getBytecodeNum();
 			temp = baseStrategy[baseIndex];
 			if (temp.startRound > round) break;
-			if (temp.endRound < round) break;
-			roundStrategyBackup[roundSize++] = temp;
+			if (temp.endRound < round) continue;
+//			roundStrategyBackup[roundSize++] = temp;
+			roundStrategy[roundSize++] = temp;
 			if (temp.priority>best.priority)
 			{
 				best = temp;
 				bestindex = roundSize;
 			}
-			System.out.println("process 1 took: "+(Clock.getBytecodeNum()-bytecode-7));
+//			System.out.println("process 1 took: "+(Clock.getBytecodeNum()-bytecode-7));
 //			System.out.println("adding strategy "+temp);
 		}
-		int bytecode = Clock.getBytecodeNum();
-		System.arraycopy(roundStrategyBackup, 0, roundStrategy, 0, roundSize);
-		System.out.println("copy took: "+(Clock.getBytecodeNum()-bytecode-7));
+//		int bytecode = Clock.getBytecodeNum();
+//		System.arraycopy(roundStrategyBackup, 0, roundStrategy, 0, roundSize);
+		System.arraycopy(roundStrategy, 0, roundStrategyBackup, 0, roundSize);
+//		System.out.println("copy took: "+(Clock.getBytecodeNum()-bytecode-7));
 //		System.out.println("total strategies: "+roundSize);
 	}
 	
@@ -80,7 +83,7 @@ public abstract class FixedStrategy extends Strategy {
 			else if (temp.endRound < round)
 			{
 				roundStrategyBackup[x] = null;
-			} else if (temp.priority<=best.priority)
+			} else if (temp.priority>best.priority)
 			{
 				best = temp;
 				bestindex = x;
@@ -89,6 +92,32 @@ public abstract class FixedStrategy extends Strategy {
 		}
 //		System.out.println("cleanup entire loop took: "+(Clock.getBytecodeNum()-bytecode-7));
 //		roundStrategy = newround;
+//		System.out.println("cleanup took: "+(Clock.getBytecodeNum()-bytecode-7));
+	}
+	
+	public void cleanUp2(int round)
+	{
+//		int bytecode = Clock.getBytecodeNum();
+		final int oldrs = roundSize;
+		StrategyElement temp;
+		StrategyElement[] newround = new StrategyElement[ARRAYLEN];
+//		System.out.println("cleanup1 took: "+(Clock.getBytecodeNum()-bytecode-7));
+		roundSize = 0;
+		best = WORST_STRATEGY;
+		for (int x=0; x<oldrs; x++)
+		{
+			temp = roundStrategyBackup[x];
+			if (temp==null || temp.endRound < round) continue;
+			else if (temp.priority>best.priority)
+			{
+				best = temp;
+				bestindex = x;
+			}
+			newround[roundSize++]=temp;
+//			System.out.println("cleanuploop took: "+(Clock.getBytecodeNum()-bytecode-7));
+		}
+//		System.out.println("cleanup entire loop took: "+(Clock.getBytecodeNum()-bytecode-7));
+		roundStrategy = newround;
 //		System.out.println("cleanup took: "+(Clock.getBytecodeNum()-bytecode-7));
 	}
 	
@@ -142,8 +171,8 @@ public abstract class FixedStrategy extends Strategy {
 //			by the time we act, we should already have a best strategy
 			exec = best;
 			best = WORST_STRATEGY;
-			roundStrategy[bestindex] = null;
 			if (execute(exec.action)) return;
+			roundStrategy[bestindex] = null;
 			
 			findBestStrategy();
 			if (best==WORST_STRATEGY) return;
