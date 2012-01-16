918cadfc59aedf36a729c79ba01bef25944f5072
Justin Venezuela <jven@mit.edu>
2012-01-15 23:10:35 -0500

    Deleted ArchonRobotYP

--- teams/ducks/RobotPlayer.java
+++ teams/ducks/RobotPlayer.java
@@ -9,7 +9,6 @@ public class RobotPlayer {
 		try {
 			switch (myRC.getType()) {
 			case ARCHON:
-				br = new ArchonRobotYP(myRC);
 //				br = new ArchonRobotHT(myRC);
 //				br = new ArchonStrategy(myRC);
 				break;
