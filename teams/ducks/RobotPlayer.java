6115610819e1cc776779ee14d0aef65b373e21ac
DarkArchon <DarkArchon@localhost>
2012-01-18 17:23:25 -0500

    flux balance uses radar, also, adding my own bot WIP

--- teams/ducks/RobotPlayer.java
+++ teams/ducks/RobotPlayer.java
@@ -1,6 +1,7 @@
 package ducks;
 
 import battlecode.common.RobotController;
+import battlecode.common.Team;
 
 public class RobotPlayer {
 	public static void run(RobotController myRC) {
@@ -15,13 +16,34 @@ public class RobotPlayer {
 			case ARCHON:
 				if(owner.equals("haitao")) br = new ArchonRobotHT(myRC);
 				else if(owner.equals("justin"))  br = new ArchonRobotJV(myRC);
-				else if(owner.equals("yp"))  br = new ArchonRobotYP_SwarmTest(myRC);
-				else br = new ArchonRobot(myRC);
+				else if(owner.equals("yp")) 
+				{
+//					br = new ArchonRobotYP_SwarmTest(myRC);
+//					br = new ArchonRobotYP(myRC);
+					if (br==null)
+					{
+						if (myRC.getTeam()==Team.A)
+							br = new ArchonRobotHT(myRC);
+						else
+							br = new ArchonRobotJV(myRC);
+					}
+					
+				} else br = new ArchonRobot(myRC);
 				break;
 			case SOLDIER:
 				if(owner.equals("haitao")) br = new SoldierRobotHT(myRC);
 				else if(owner.equals("justin")) br = new SoldierRobotJV(myRC);
-				else br = new SoldierRobot(myRC);
+				else if (owner.equals("yp"))
+				{
+//					br = new SoldierRobotYP(myRC);
+					if (br == null)
+					{
+						if (myRC.getTeam()==Team.A)
+							br = new SoldierRobotHT(myRC);
+						else
+							br = new SoldierRobotJV(myRC);
+					}
+				} else br = new SoldierRobot(myRC);
 				break;
 			case SCOUT:
 				break;
---
+++ teams/ducks/SoldierRobotYP.java
@@ -0,0 +1,17 @@
+package ducks;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class SoldierRobotYP extends BaseRobot {
+
+	public SoldierRobotYP(RobotController myRC) {
+		super(myRC);
+	}
+
+	@Override
+	public void run() throws GameActionException {
+		
+	}
+
+}
