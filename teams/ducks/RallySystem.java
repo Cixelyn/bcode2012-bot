7db8792fc5555c8fa2ab9ce0466bce94d5bf0e02
Justin Venezuela <jven@mit.edu>
2012-01-17 22:45:08 -0500

    Fixed bug where Archons were not subscribed to explorer channel

--- teams/ducks/RallySystem.java
+++ teams/ducks/RallySystem.java
@@ -5,36 +5,25 @@ import battlecode.common.MapLocation;
 /**
  * This system is used to communicate objective information between units.
  * @author jven
- *
  */
 public class RallySystem {
 
 	private BaseRobot br;
 	private int timeUntilBroadcast;
-	private MapLocation objective;
 	
 	public RallySystem(BaseRobot myBR) {
 		br = myBR;
 		timeUntilBroadcast = Constants.ARCHON_BROADCAST_FREQUENCY;
-		objective = br.myHome;
 	}
 	
 	public MapLocation getCurrentObjective() {
-		return objective;
+		return br.mc.guessEnemyPowerCoreLocation();
 	}
 	
 	public void broadcastRally() {
 		if (--timeUntilBroadcast <= 0) {
-			// share exploration information
-			br.ses.broadcastMapEdges();
-			br.ses.broadcastMapFragment();
-			br.ses.broadcastPowerNodeFragment();
-			br.io.sendMapLoc("#xr", br.mc.guessEnemyPowerCoreLocation());
+			br.io.sendShort("#xr", 0);
 			timeUntilBroadcast = Constants.ARCHON_BROADCAST_FREQUENCY;
 		}
-	}
-	
-	public void processRally(MapLocation rally) {
-		objective = rally;
 	}
 }
