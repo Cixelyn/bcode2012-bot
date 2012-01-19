09e139a4c71a0bac3f776b240860ac1840dd979b
hmao5 <947979+hmao5@users.noreply.github.com>
2012-01-19 04:57:45 -0500

    push me in the face

---
+++ teams/team047/BroadcastType.java
@@ -0,0 +1,50 @@
+package team047;
+
+public enum BroadcastType {
+	
+	// shared exploration
+	POWERNODE_FRAGMENTS,
+	MAP_FRAGMENTS,
+	MAP_EDGES,
+
+	// random shit
+	OWNERSHIP_CLAIM,
+	ENEMY_ARCHON_KILL,
+	
+	// scout wire system
+	WIRE_REQUEST,
+	WIRE_ACCEPT,
+	WIRE_CONFIRM,
+	WIRE_ABORT,
+
+	// rally code
+	RALLY,
+	
+	// swarm code
+	ANNOUNCE_ENEMY,
+	// (temporary)
+	SWARM_DETAILS,
+	
+	
+	// ushorts(15-bit), [seek_or_swarm, target_loc.x, target_loc.y, sender_loc.x, sender_loc.y]
+	SWARM_TARGET,
+	
+	
+	
+	
+	
+	
+	
+	
+//	ending semicolon
+	;
+	
+	public char header;
+	BroadcastType() {
+		header = (char)(this.ordinal() + 0x100);
+	}
+	
+	public static BroadcastType decode(char header) {
+		return BroadcastType.values()[header - 0x100];
+	}
+}
---
+++ teams/team047/StrategyRobotExtended.java
@@ -0,0 +1,107 @@
+package team047;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public abstract class StrategyRobotExtended extends BaseRobot {
+
+	private final static int MAX_STATE_STACK_SIZE = 10;
+	
+	private final RobotState[] stateStack;
+	private int stack_size;
+	private RobotState curState;
+	
+	public StrategyRobotExtended(RobotController myRC, RobotState initialState) throws GameActionException {
+		super(myRC);
+		stateStack = new RobotState[MAX_STATE_STACK_SIZE];
+		stack_size = 0;
+		curState = initialState;
+	}
+
+	@Override
+	public void run() throws GameActionException {
+
+		initializeForRound();
+		
+		gotoState(processTransitions(curState));
+		execute(curState);
+	}
+	
+	
+	/**
+	 * Pushes the given state over the current state. 
+	 * Saves the current state to the state stack.
+	 * Should only be called to push a different state than the current state,
+	 * on successful push, will call gotoState(newState)
+	 */
+	protected void pushState(RobotState newState) throws GameActionException
+	{
+		if (curState == newState)
+		{
+		} else
+		{
+			stateStack[stack_size++] = curState;
+			gotoState(newState);
+		}
+	}
+	
+	/**
+	 * Pops the last state from the stateStack and calls gotoState() on it
+	 */
+	protected void popState() throws GameActionException
+	{
+		if (stack_size == 0)
+		{
+		} else
+		{
+			gotoState(stateStack[--stack_size]);
+		}
+	}
+
+
+	/**
+	 * Returns the current Robot's state
+	 * @return
+	 */
+	public RobotState getCurrentState() {
+		return curState;
+	}
+	
+	/**
+	 * returns which state to transition to.
+	 * (return the same state if not transitioning
+	 * Use this to setup the normal state transition diagram
+	 */
+	public abstract RobotState processTransitions(RobotState state) throws GameActionException;
+
+	
+	
+	/**
+	 * ONLY CALL WHEN PROCESSING MESSAGES OTHERWISE YOU GET G'ED
+	 * Because decoupling is impossible for 
+	 * @param newstate - state you want to switch into
+	 * @throws GameActionException
+	 */
+	public void gotoState(RobotState newstate) throws GameActionException {
+		if (newstate!=curState)
+		{
+			prepareTransition(newstate, curState);
+			curState = newstate;
+		}
+	}
+
+	/**
+	 * do any special things you need to do at the beginning at a new round
+	 */
+	public abstract void initializeForRound() throws GameActionException;
+	
+	/**
+	 * set up fields for transitionoing from oldstate to newstate
+	 */
+	public abstract void prepareTransition(RobotState newstate, RobotState oldstate) throws GameActionException;
+	
+	/**
+	 * execute the behavior for the given state
+	 */
+	public abstract void execute(RobotState state) throws GameActionException;
+}
