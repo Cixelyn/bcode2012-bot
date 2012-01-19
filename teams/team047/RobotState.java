483b3ee02a72abca68ae3ddd6bd2ae4bda684f83
hmao5 <hmao5@localhost>
2012-01-19 04:57:45 -0500

    push me in the face

---
+++ teams/team047/RobotState.java
@@ -0,0 +1,29 @@
+package team047;
+
+public enum RobotState {
+	INITIALIZE,
+	EXPLORE,
+	GOHOME,
+	BUILD_ARMY,
+	ATTACK_MOVE_INITIAL, ATTACK_ENEMY_BASE,
+	DEFEND_BASE,
+	POWER_CAP,
+	
+	// soldier bots states
+	HOLD_POSITION,
+	HIBERNATE,
+	CHASE,
+	SWARM,
+	ROAM,
+	SUICIDE,
+	
+	// to delete errors
+	DEFEND,
+	GOTO_POWER_CORE,
+	SPAWN_SOLDIERS,
+	SPLIT,
+	POWER_SAVE,
+	RUSH,
+//		CHASE,
+	BACK_OFF
+}
