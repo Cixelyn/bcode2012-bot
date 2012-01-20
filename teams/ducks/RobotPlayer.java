5fffa5fce09965b0efca47e7323d242c42ba37c3
Justin Venezuela <jven@mit.edu>
2012-01-20 18:23:19 -0500

    Refactoring old wire code. Wrote new scout robot.

--- teams/ducks/RobotPlayer.java
+++ teams/ducks/RobotPlayer.java
@@ -15,7 +15,7 @@ public class RobotPlayer {
 			switch (myRC.getType()) {
 			case ARCHON:
 				if(owner.equals("haitao")) br = new ArchonRobotHT(myRC);
-				else if(owner.equals("justin"))  br = new ArchonRobotJV2(myRC);
+				else if(owner.equals("justin"))  br = new ArchonRobotJV(myRC);
 				else if(owner.equals("cory")) br = new ArchonRobotCL(myRC);
 				else if(owner.equals("yp")) 
 				{
@@ -40,7 +40,7 @@ public class RobotPlayer {
 				break;
 			case SOLDIER:
 				if(owner.equals("haitao")) br = new SoldierRobotHT(myRC);
-				else if(owner.equals("justin")) br = new SoldierRobotJV(myRC);
+				else if(owner.equals("justin")) br = null;
 				else if(owner.equals("cory")) br = new SoldierRobotCL(myRC);
 				else if (owner.equals("yp"))
 				{
@@ -61,7 +61,7 @@ public class RobotPlayer {
 				} else br = new SoldierRobot(myRC);
 				break;
 			case SCOUT:
-				if (owner.equals("justin")) br = new ScoutRobotJV2(myRC);
+				if (owner.equals("justin")) br = new ScoutRobotJV(myRC);
 				else if (owner.equals("cory")) br = new ScoutRobotCL(myRC);
 				else if (owner.equals("yp"))
 				{
