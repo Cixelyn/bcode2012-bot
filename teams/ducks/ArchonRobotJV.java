afcc278f08d15b6c84079a5dcf1a2166bb1d5991
Justin Venezuela <jven@jvenezue.la>
2012-01-21 01:04:18 -0500

    Implemented space filling curves for scout exploration

--- teams/ducks/ArchonRobotJV.java
+++ teams/ducks/ArchonRobotJV.java
@@ -12,42 +12,40 @@ public class ArchonRobotJV extends BaseRobot {
 	
 	private enum StrategyState {
 		SPLIT,
-		RUSH
-	}
-	
-	private enum BehaviorState {
-		SWARM,
-		ENGAGE,
-		CHASE
+		MAKE_SCOUTS
 	}
 	
 	private static class MyConstants {
 		public static final int ROUND_TO_STOP_SPLIT = 20;
-		public static final int SPLIT_DISTANCE = 20;
 	}
 	
 	private StrategyState strategy;
-	private BehaviorState behavior;
+	
+	int numScouts;
 
 	public ArchonRobotJV(RobotController myRC) throws GameActionException {
 		super(myRC);
 		// set initial states
 		strategy = StrategyState.SPLIT;
-		behavior = BehaviorState.SWARM;
-		// set navigation mode
-		nav.setNavigationMode(NavigationMode.TANGENT_BUG);
-		nav.setDestination(myHome);
+		// set flux balance mode
+		fbs.setPoolMode();
+		// set broadcast channels
+		io.setChannels(new BroadcastChannel[] {
+				BroadcastChannel.ALL,
+				BroadcastChannel.ARCHONS,
+				BroadcastChannel.EXPLORERS
+		});
 	}
 
 	@Override
 	public void run() throws GameActionException {
+		// scan
+		radar.scan(true, true);
 		// check strategy state
-		if (curRound > MyConstants.ROUND_TO_STOP_SPLIT) {
-			strategy = StrategyState.RUSH;
-		}
-		// set target
-		if (strategy == StrategyState.RUSH) {
-			nav.setDestination(mc.guessEnemyPowerCoreLocation());
+		if (curRound < MyConstants.ROUND_TO_STOP_SPLIT) {
+			strategy = StrategyState.SPLIT;
+		} else {
+			strategy = StrategyState.MAKE_SCOUTS;
 		}
 	}
 	
@@ -71,54 +69,47 @@ public class ArchonRobotJV extends BaseRobot {
 	
 	@Override
 	public MoveInfo computeNextMove() throws GameActionException {
-		if (strategy == StrategyState.SPLIT) {
-			// move away from home
-			return new MoveInfo(myHome.directionTo(birthplace), false);
-		} else if (strategy == StrategyState.RUSH) {
-			// check if i should build a scout
-			if (rc.getFlux() >= RobotType.SCOUT.spawnCost +
-					0.4 * RobotType.SCOUT.maxFlux) {
-				if (rc.senseTerrainTile(curLocInFront) != TerrainTile.OFF_MAP &&
-						rc.senseObjectAtLocation(
-						curLocInFront, RobotLevel.IN_AIR) == null) {
-					return new MoveInfo(RobotType.SCOUT, curDir);
+		switch (strategy) {
+			case SPLIT:
+				return new MoveInfo(myHome.directionTo(birthplace), false);
+			case MAKE_SCOUTS:
+				if (rc.getFlux() >= RobotType.SCOUT.spawnCost +
+						RobotType.SCOUT.maxFlux) {
+					if (rc.senseTerrainTile(curLocInFront) != TerrainTile.OFF_MAP &&
+							rc.senseObjectAtLocation(
+							curLocInFront, RobotLevel.IN_AIR) == null) {
+						return new MoveInfo(RobotType.SCOUT, curDir);
+					} else {
+						return new MoveInfo(curDir.rotateLeft());
+					}
+				} else {
+					return new MoveInfo(curLoc.directionTo(
+							mc.guessEnemyPowerCoreLocation()));
 				}
-			}
-			// move towards target
-			MapLocation closestArchon = dc.getClosestArchon();
-			if (closestArchon != null) {
-				int distance = curLoc.distanceSquaredTo(closestArchon);
-				if (Math.random() < 0.6 * (MyConstants.SPLIT_DISTANCE - distance) /
-						MyConstants.SPLIT_DISTANCE) {
-					return new MoveInfo(
-							curLoc.directionTo(dc.getClosestArchon()).opposite(), false);
-				}
-			}
-			return new MoveInfo(nav.navigateToDestination(), true);
-		} else {
-			return null;
+			default:
+				return null;
 		}
 	}
 	
 	@Override
-	public void useExtraBytecodes() {
-		// TODO(jven): check we didn't go over bytecodes within the method
-		// think about where to move
-		while (Clock.getBytecodesLeft() > 3000) {
-			nav.prepare();
-		}
+	public void useExtraBytecodes() throws GameActionException {
+		super.useExtraBytecodes();
 		// share exploration
-		if (Clock.getBytecodesLeft() > 3000 && Math.random() < 0.3) {
+		if (curRound == Clock.getRoundNum() &&
+				Clock.getBytecodesLeft() > 3000 && Math.random() < 0.3) {
 			ses.broadcastMapFragment();
 		}
-		if (Clock.getBytecodesLeft() > 1000 && Math.random() < 0.3) {
+		if (curRound == Clock.getRoundNum() &&
+				Clock.getBytecodesLeft() > 1000 && Math.random() < 0.3) {
 			ses.broadcastPowerNodeFragment();
 		}
-		if (Clock.getBytecodesLeft() > 1000 && Math.random() < 0.3) {
+		if (curRound == Clock.getRoundNum() &&
+				Clock.getBytecodesLeft() > 1000 && Math.random() < 0.3) {
 			ses.broadcastMapEdges();
 		}
 		// process shared exploration
-		while (Clock.getBytecodesLeft() > 500) {
+		while (curRound == Clock.getRoundNum() &&
+				Clock.getBytecodesLeft() > 1000) {
 			mc.extractUpdatedPackedDataStep();
 		}
 	}
