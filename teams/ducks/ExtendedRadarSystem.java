bc7968c377cd6081bee01b6a07b6aff0dd9eefdc
hmao5 <947979+hmao5@users.noreply.github.com>
2012-01-20 22:03:18 -0500

    cccccccccccccc

--- teams/ducks/ExtendedRadarSystem.java
+++ teams/ducks/ExtendedRadarSystem.java
@@ -7,26 +7,37 @@ public class ExtendedRadarSystem {
 	private static final int BUFFER_SIZE = 4096;
 	
 	private final BaseRobot br;
-	public final MapLocation[] locationInfo;
-	public final int[] energonInfo;
-	private final FastIDSet keySet;
+	private final MapLocation[] enemyLocationInfo;
+	private final int[] enemyEnergonInfo;
+	private final FastIDSet enemyKeySet;
+	private final MapLocation[] allyLocationInfo;
+	private final int[] allyEnergonInfo;
+	private final FastIDSet allyKeySet;
 	public ExtendedRadarSystem(BaseRobot br) {
 		this.br = br;
-		locationInfo = new MapLocation[BUFFER_SIZE];
-		energonInfo = new int[BUFFER_SIZE];
-		keySet = new FastIDSet(5);
+		enemyLocationInfo = new MapLocation[BUFFER_SIZE];
+		enemyEnergonInfo = new int[BUFFER_SIZE];
+		enemyKeySet = new FastIDSet(5);
+		allyLocationInfo = new MapLocation[BUFFER_SIZE];
+		allyEnergonInfo = new int[BUFFER_SIZE];
+		allyKeySet = new FastIDSet(5);
 	}
 	
 	/** Ally broadcasted some enemy info to us, integrate it into the system. <br>
 	 * Should only be called from processMessage().
 	 */
 	public void integrateEnemyInfo(int[] info) {
-		for(int n=0; n<info.length; n+=4) {
+		int senderID = info[0] % BUFFER_SIZE;
+		allyLocationInfo[senderID] = new MapLocation(info[1], info[2]);
+		allyEnergonInfo[senderID] = info[3];
+		allyKeySet.addID(senderID);
+		
+		for(int n=4; n<info.length; n+=4) {
 			int id = info[n] % BUFFER_SIZE;
-			locationInfo[id] = new MapLocation(info[n+1], info[n+2]);
-			energonInfo[id] = info[n+3];
+			enemyLocationInfo[id] = new MapLocation(info[n+1], info[n+2]);
+			enemyEnergonInfo[id] = info[n+3];
 			
-			keySet.addID(id);
+			enemyKeySet.addID(id);
 		}
 	}
 	
@@ -34,7 +45,7 @@ public class ExtendedRadarSystem {
 	 * Should only be called from processMessage().
 	 */
 	public void integrateEnemyKill(int killID) {
-		keySet.removeID(killID % BUFFER_SIZE);
+		enemyKeySet.removeID(killID % BUFFER_SIZE);
 	}
 	
 	/** Gets info about a kill into your own and nearby robots' extended radar. */
@@ -45,19 +56,39 @@ public class ExtendedRadarSystem {
 	
 	/** Adds block of enemy info from this turn and removes block that timed out. */
 	public void step() {
-		keySet.endRound();
+		enemyKeySet.endRound();
+	}
+	
+	/** Returns how much more energon worth of robots we have in the given radius. 
+	 * Returns a positive number iff we have more energon than the other team.
+	 */
+	public int getEnergonDifference(int radiusSquared) {
+		int diff = 0;
+		int size = enemyKeySet.size();
+		for(int i=0; i<size; i++) {
+			int id = enemyKeySet.getID(i);
+			if(br.curLoc.distanceSquaredTo(enemyLocationInfo[id]) <= radiusSquared) 
+				diff -= enemyEnergonInfo[id];
+		}
+		size = allyKeySet.size();
+		for(int i=0; i<size; i++) {
+			int id = allyKeySet.getID(i);
+			if(br.curLoc.distanceSquaredTo(allyLocationInfo[id]) <= radiusSquared) 
+				diff += allyEnergonInfo[id];
+		}
+		return diff;
 	}
 	
 	/** Returns the location of this closest enemy. */
 	public MapLocation getClosestEnemyLocation() {
-		int size = keySet.size();
+		int size = enemyKeySet.size();
 		MapLocation ret = null;
 		int bestDist = Integer.MAX_VALUE;
 		for(int i=0; i<size; i++) {
-			int id = keySet.getID(i);
-			int dist = br.curLoc.distanceSquaredTo(locationInfo[id]);
+			int id = enemyKeySet.getID(i);
+			int dist = br.curLoc.distanceSquaredTo(enemyLocationInfo[id]);
 			if(dist<bestDist) {
-				ret = locationInfo[id];
+				ret = enemyLocationInfo[id];
 				bestDist = dist;
 			}
 		}
@@ -67,10 +98,10 @@ public class ExtendedRadarSystem {
 	/** Returns a direction to go that will most likely bring you to an enemy. */
 	public Direction getDirectionWithMostEnemies() {
 		int[] counts = new int[8];
-		int size = keySet.size();
+		int size = enemyKeySet.size();
 		for(int i=0; i<size; i++) {
-			int id = keySet.getID(i);
-			int dirOrdinal = br.curLoc.directionTo(locationInfo[id]).ordinal();
+			int id = enemyKeySet.getID(i);
+			int dirOrdinal = br.curLoc.directionTo(enemyLocationInfo[id]).ordinal();
 			if(dirOrdinal<8) {
 				counts[(dirOrdinal+1)%8]++;
 				counts[dirOrdinal]+=2;
