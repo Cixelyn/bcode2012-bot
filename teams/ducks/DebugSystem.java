1e81b32f841f4ee98508e51592fdee795860e662
Cory Li <coryli@mit.edu>
2012-01-22 02:58:46 -0500

    fuyck me

--- teams/ducks/DebugSystem.java
+++ teams/ducks/DebugSystem.java
@@ -15,11 +15,11 @@ public class DebugSystem {
 	}
 	
 	public void println(char owner, String msg) {
-		if(launch_owner == owner || launch_owner == 'e')
+		if(launch_owner == owner || owner == 'e')
 			System.out.println(msg);
 	}
 	public void setIndicatorString(char owner, int position, String msg) {
-		if(launch_owner == owner || launch_owner == 'e') {
+		if(launch_owner == owner || owner == 'e') {
 			br.rc.setIndicatorString(position,msg);
 		}
 	}
