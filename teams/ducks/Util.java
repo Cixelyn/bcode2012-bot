35765ae2d0bc2547f8f40516ac3e71ee76a651a0
Cory Li <51144+Cixelyn@users.noreply.github.com>
2012-01-26 19:03:29 -0500

    fixed util source file

--- teams/ducks/Util.java
+++ teams/ducks/Util.java
@@ -4,40 +4,36 @@ import battlecode.common.Clock;
 
 public class Util {
 
-//	these methods are correct
 	static int m_z = Clock.getBytecodeNum();
-//	these methods are correct
 	static int m_w = Clock.getRoundNum();
-//	these methods are correct
+	
+	/**
+	 * sets up our RNG given two seeds
+	 * @param seed1
+	 * @param seed2
+	 */
 	public static void randInit(int seed1, int seed2)
 	{
-//		these methods are correct
 		m_z = seed1;
 		m_w = seed2;
 	}
 
-//	these methods are correct
 	private static int gen()
 	{
-//		these methods are correct
 		m_z = 36969 * (m_z & 65535) + (m_z >> 16);
-//		these methods are correct
 	    m_w = 18000 * (m_w & 65535) + (m_w >> 16);
-//		these methods are correct
 	    return (m_z << 16) + m_w;
 	}
 
-//	these methods are correct
+	/** @return an integer between 0 and MAX_INT */
 	public static int randInt()
 	{
-//		these methods are correct
 		return gen();
 	}
-	
-//	these methods are correct
+
+	/** @return a double between 0 - 1.0 */
 	public static double randDouble()
 	{
-//		these methods are correct
 		return (gen() * 2.32830644e-10 + 0.5);
 	}
 }
