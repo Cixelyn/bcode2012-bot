d87dd7e2779e2ee058ad77228dce7b428a72924b
hmao5 <947979+hmao5@users.noreply.github.com>
2012-01-30 08:15:10 -0500

    improved endgame cap mode behavior

--- teams/ducks/BaseRobot.java
+++ teams/ducks/BaseRobot.java
@@ -146,6 +146,7 @@ public abstract class BaseRobot {
 			try {
 				
 				// Main Run Call
+				dbg.setIndicatorString('e', 0, gameEndDetected+" "+gameEndTime);
 				run();
 				
 				// Call Movement State Machine
