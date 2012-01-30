2aecbaf39b0efa3a8fd1bf9f51144b705be8bb72
Cory Li <51144+Cixelyn@users.noreply.github.com>
2012-01-30 16:34:36 -0500

    endgame cap mode buffer pushed into gameconstants

--- teams/ducks/Constants.java
+++ teams/ducks/Constants.java
@@ -12,5 +12,8 @@ public final class Constants {
 
 	/** Numbers of rounds of not seeing an enemy before resetting targets */
 	public static final int ENEMY_SPOTTED_SIGNAL_TIMEOUT = 50;
+
+	/** Rounds before the end of the game to go into emergency endgame cap mode */
+	public static final int ENDGAME_CAP_MODE_BUFFER = 2000;
 	
 }
